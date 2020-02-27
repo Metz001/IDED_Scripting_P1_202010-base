5ccc9c7cb427d470bea27127ef1f41ad6999a12e
Metz001 <46971952+Metz001@users.noreply.github.com>
2020-02-27 07:57:39 -0500

    Nuevos Scripts Parcial
    Modificaciones para la entrega del parcial I

--- IDED_Scripting_P1_202010-base/Logic/Human.cs
+++ IDED_Scripting_P1_202010-base/Logic/Human.cs
@@ -2,17 +2,30 @@
 {
     public class Human : Unit
     {
-        public float Potential { get; set; }
+        public float Potential { get; set; }        //Potencial, disponible únicamente para Humanos
 
         public Human(EUnitClass _unitClass, int _atk, int _def, int _spd, int _moveRange, float _potential)
             : base(_unitClass, _atk, _def, _spd, _moveRange)
         {
-            Potential = _potential;
+            Potential = _potential;         //El potencial debe de aumentar las habilidades de atk y def de los humanos
+           Attack = _atk * _potential;
+            Defense = _def * _potential;
+            Speed = _spd * _potential;
+
+
         }
 
         public virtual bool ChangeClass(EUnitClass newClass)
         {
-            return false;
+            if (UnitClass == EUnitClass.Soldier && newClass == EUnitClass.Squire)
+                return true;
+            if (UnitClass == EUnitClass.Squire && newClass == EUnitClass.Soldier)
+                return true;
+            if (UnitClass == EUnitClass.Mage && newClass == EUnitClass.Ranger)
+                return true;
+            if (UnitClass == EUnitClass.Ranger && newClass == EUnitClass.Mage)
+                return true;
+            return false;           //método para cambiar libremente entre clases, Menos el Aldeano
         }
     }
 }
--- IDED_Scripting_P1_202010-base/Logic/Unit.cs
+++ IDED_Scripting_P1_202010-base/Logic/Unit.cs
@@ -3,19 +3,19 @@
     public class Unit
     {
         public int BaseAtk { get; protected set; }
-        public int BaseDef { get; protected set; }
+        public int BaseDef { get; protected set; }          //Las unidades tiene Atk Def y sus multiplicadores
         public int BaseSpd { get; protected set; }
 
-        public int MoveRange { get; protected set; }
+        public int MoveRange { get; protected set; }  //tmb atributos de Range
         public int AtkRange { get; protected set; }
 
-        public float BaseAtkAdd { get; protected set; }
+        public float BaseAtkAdd { get; protected set; }   //Multiplicadores, dependen de su UnitClass
         public float BaseDefAdd { get; protected set; }
         public float BaseSpdAdd { get; protected set; }
 
-        public float Attack { get; }
-        public float Defense { get; }
-        public float Speed { get; }
+        public float Attack { get; protected set; }
+        public float Defense { get; protected set; }               //Resultado de Base * % de Add
+        public float Speed { get; protected set; }
 
         protected Position CurrentPosition;
 
@@ -23,20 +23,66 @@
 
         public Unit(EUnitClass _unitClass, int _atk, int _def, int _spd, int _moveRange)
         {
+           
             UnitClass = _unitClass;
-            BaseAtk = _atk;
+            BaseAtk = _atk;                     //Constructor
             BaseDef = _def;
             BaseSpd = _spd;
             MoveRange = _moveRange;
+            if (_unitClass == EUnitClass.Mage || _unitClass == EUnitClass.Ranger)
+                AtkRange = 3;
+            else if (_unitClass == EUnitClass.Dragon)
+                AtkRange = 5;
+            else if (_unitClass == EUnitClass.Villager)
+            {
+                BaseAtk = 0;
+                BaseDef = 0;
+                BaseSpd = _spd;
+            }
+
         }
 
         public virtual bool Interact(Unit otherUnit)
         {
-            return false;
+            bool result = true;
+
+            if (otherUnit.UnitClass == EUnitClass.Villager)
+            {
+                result = false;
+                return result;
+            }         
+            if (UnitClass == EUnitClass.Villager && otherUnit.UnitClass != EUnitClass.Squire || otherUnit.UnitClass != EUnitClass.Soldier|| otherUnit.UnitClass != EUnitClass.Villager)
+            {
+                result = false;
+            }
+
+            if (UnitClass == EUnitClass.Mage)
+                if (otherUnit.UnitClass == EUnitClass.Mage || otherUnit.UnitClass == EUnitClass.Ranger)
+                    result = false;
+            if (UnitClass == EUnitClass.Dragon)
+                if (otherUnit.UnitClass == EUnitClass.Imp || otherUnit.UnitClass == EUnitClass.Ranger || otherUnit.UnitClass == EUnitClass.Orc)
+                    result = false;
+
+            return result;                             //Método de interactiar con otra unidad
+                                                         //¿que puede hacer esta unidad con "otherUnit"?
         }
 
-        public virtual bool Interact(Prop prop) => false;
+        public virtual bool Interact(Prop prop)
+        {
+            bool result = false;
+            if (UnitClass == EUnitClass.Villager)
+                result = true;
+            return result;
 
-        public bool Move(Position targetPosition) => false;
+        }
+
+        public bool Move(Position targetPosition)
+        {
+            bool result = false;
+         
+
+            return result;
+        } 
+
     }
 }
