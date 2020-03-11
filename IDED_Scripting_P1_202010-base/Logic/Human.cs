e9203882f1ffc224116a706a2a7db0c03816e0d5
Metz001 <46971952+Metz001@users.noreply.github.com>
2020-03-10 20:44:16 -0500

    Add files via upload

--- IDED_Scripting_P1_202010-base/Logic/Human.cs
+++ IDED_Scripting_P1_202010-base/Logic/Human.cs
@@ -7,10 +7,64 @@
         public Human(EUnitClass _unitClass, int _atk, int _def, int _spd, int _moveRange, float _potential)
             : base(_unitClass, _atk, _def, _spd, _moveRange)
         {
-            Potential = _potential;         //El potencial debe de aumentar las habilidades de atk y def de los humanos
-           Attack = _atk * _potential;
-            Defense = _def * _potential;
-            Speed = _spd * _potential;
+            UnitClass = _unitClass;
+            BaseAtk = _atk;                     //Constructor
+            BaseDef = _def;
+            BaseSpd = _spd;
+            MoveRange = _moveRange;
+
+            if (_unitClass == EUnitClass.Dragon || _unitClass == EUnitClass.Orc || _unitClass == EUnitClass.Imp)
+            {
+                UnitClass = EUnitClass.Villager;
+                BaseAtk = 0;
+                BaseDef = 0;
+            }
+                                     // Verificación, que no sea un monstruo humano
+            if (_unitClass == EUnitClass.Villager)
+            {
+                BaseAtk = 0;
+                BaseDef = 0;
+            }
+
+            if (_unitClass == EUnitClass.Mage || _unitClass == EUnitClass.Ranger)
+                AtkRange = 3;
+            else
+                AtkRange = 1;
+
+            switch (UnitClass)
+            {
+                case (EUnitClass.Squire):
+                    BaseAtkAdd = 0.2f;
+                    BaseDefAdd = 0.1f;
+                    BaseSpdAdd = 0f;
+                    break;
+                case (EUnitClass.Soldier):
+                    BaseAtkAdd = 0.3f;
+                    BaseDefAdd = 0.2f;
+                    BaseSpdAdd = 1f;
+                    break;
+                case (EUnitClass.Ranger):
+                    BaseAtkAdd = 0.1f;
+                    BaseDefAdd = 0f;
+                    BaseSpdAdd = 0.3f;
+                    break;
+                case (EUnitClass.Mage):
+                    BaseAtkAdd = 0.3f;
+                    BaseDefAdd = 0.1f;
+                    BaseSpdAdd = -0.1f;
+                    break;
+            }
+
+            Attack = (BaseAtkAdd * BaseAtk) + (BaseAtk*Potential)+BaseAtk;
+            if (Attack > 255)
+                Attack = 255;
+            Defense = (BaseDefAdd * BaseDef) + (BaseDef * Potential) + BaseDef;  //El potencial debe de aumentar las habilidades de atk y def de los humanos
+            if (Defense > 255)
+                Defense = 255;
+            Speed = (BaseSpdAdd * BaseSpd) + (BaseSpd*Potential)+ BaseSpd;
+            if (Speed > 255)
+                Speed = 255;
+
 
 
         }
@@ -18,13 +72,29 @@
         public virtual bool ChangeClass(EUnitClass newClass)
         {
             if (UnitClass == EUnitClass.Soldier && newClass == EUnitClass.Squire)
+            {
+                UnitClass = EUnitClass.Squire;
                 return true;
+            }
+               
             if (UnitClass == EUnitClass.Squire && newClass == EUnitClass.Soldier)
+            {
+                UnitClass = EUnitClass.Soldier;
                 return true;
+            }
+                
             if (UnitClass == EUnitClass.Mage && newClass == EUnitClass.Ranger)
+            {
+                UnitClass = EUnitClass.Ranger;
                 return true;
+            }
+           
             if (UnitClass == EUnitClass.Ranger && newClass == EUnitClass.Mage)
+            {
+                UnitClass = EUnitClass.Mage;
                 return true;
+            }
+            
             return false;           //método para cambiar libremente entre clases, Menos el Aldeano
         }
     }
--- IDED_Scripting_P1_202010-base/Logic/Unit.cs
+++ IDED_Scripting_P1_202010-base/Logic/Unit.cs
@@ -16,31 +16,58 @@
         public float Attack { get; protected set; }
         public float Defense { get; protected set; }               //Resultado de Base * % de Add
         public float Speed { get; protected set; }
-
-        protected Position CurrentPosition;
-
         public EUnitClass UnitClass { get; protected set; }
 
+        public int[] Cposition { get; protected set; } = new int[2];
+        
+
+      
+
         public Unit(EUnitClass _unitClass, int _atk, int _def, int _spd, int _moveRange)
-        {
-           
+        {           
             UnitClass = _unitClass;
             BaseAtk = _atk;                     //Constructor
             BaseDef = _def;
             BaseSpd = _spd;
             MoveRange = _moveRange;
-            if (_unitClass == EUnitClass.Mage || _unitClass == EUnitClass.Ranger)
-                AtkRange = 3;
-            else if (_unitClass == EUnitClass.Dragon)
+            if (MoveRange > 10)
+                MoveRange = 10;
+           
+            if (_unitClass == EUnitClass.Dragon)
                 AtkRange = 5;
-            else if (_unitClass == EUnitClass.Villager)
+            else
+                AtkRange = 1;
+           
+            switch (UnitClass)
             {
-                BaseAtk = 0;
-                BaseDef = 0;
-                BaseSpd = _spd;
+                case (EUnitClass.Imp):
+                    BaseAtkAdd = 0.1f;
+                    BaseDefAdd = 0f;
+                    BaseSpdAdd = 0f;
+                    break;
+                case (EUnitClass.Orc):
+                    BaseAtkAdd = 0.4f;
+                    BaseDefAdd = 0.2f;
+                    BaseSpdAdd = -0.2f;
+                    break;
+                case (EUnitClass.Dragon):
+                    BaseAtkAdd = 0.5f;
+                    BaseDefAdd = 0.3f;
+                    BaseSpdAdd = 0.2f;
+                    break;
             }
+            Attack = BaseAtkAdd * BaseAtk + BaseAtk;
+            if (Attack > 255)
+                Attack = 255;
+            Defense= BaseDefAdd * BaseDef+ BaseDef;
+            if (Defense > 255)
+                Defense = 255;
+            Speed = BaseSpdAdd * BaseSpd+ BaseSpd;
+            if (Speed > 255)
+                Speed = 255;
 
         }
+     
 
         public virtual bool Interact(Unit otherUnit)
         {
@@ -51,7 +78,8 @@
                 result = false;
                 return result;
             }         
-            if (UnitClass == EUnitClass.Villager && otherUnit.UnitClass != EUnitClass.Squire || otherUnit.UnitClass != EUnitClass.Soldier|| otherUnit.UnitClass != EUnitClass.Villager)
+            if (UnitClass == EUnitClass.Villager && otherUnit.UnitClass != EUnitClass.Squire || otherUnit.UnitClass != EUnitClass.Soldier|| 
+                otherUnit.UnitClass != EUnitClass.Villager)
             {
                 result = false;
             }
@@ -72,17 +100,22 @@
             bool result = false;
             if (UnitClass == EUnitClass.Villager)
                 result = true;
+            else
+                result = false;
             return result;
 
         }
-
-        public bool Move(Position targetPosition)
+        
+        public bool Move(int[] targetPosition)
         {
             bool result = false;
+
+         
          
 
             return result;
         } 
+        
 
     }
 }
